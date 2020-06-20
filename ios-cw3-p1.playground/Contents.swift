cd53a09c5bc525dd3852b9bc6c8845b77a621fcb
عبدالله مشعل المنيخ -404 <66527073+Munaikh@users.noreply.github.com>
2020-06-20 20:14:50 +0300

    Done

---
+++ ios-cw3-p1.playground/Contents.swift
@@ -0,0 +1,9 @@
+var number1 = 14
+
+var number2 = 5
+
+func add(a: Int, b: Int) -> Int {
+    a + b
+}
+var functionCall = add(a: number1, b: number2)
+print("\(number1) + \(number2) = \(functionCall)")
